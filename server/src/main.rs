1f150b5ebe1b4fee85043007a773de307242333d
Dominik Purgał <amonsir29@gmail.com>
2023-04-23 20:19:51 +0000

    Add renet configuration

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,25 @@
+use std::{net::UdpSocket, time::SystemTime};
+
+use bevy::prelude::*;
+use bevy_renet::{renet::{RenetServer, RenetConnectionConfig, ServerConfig, ServerAuthentication}, RenetServerPlugin};
+
+const PROTOCOL_ID: u64 = 7;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugin(AssetPlugin::default())        
+        .add_plugins(MinimalPlugins)
+        .add_plugin(RenetServerPlugin::default())
+        .insert_resource(new_renet_server())
+        .run();
+}
+
+fn new_renet_server() -> RenetServer {
+    let server_addr = "127.0.0.1:2137".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+    let connection_config = RenetConnectionConfig::default();
+    let server_config = ServerConfig::new(64, PROTOCOL_ID, server_addr, ServerAuthentication::Unsecure);
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    
+    RenetServer::new(current_time, server_config, connection_config, socket).unwrap()
 }
