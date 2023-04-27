d17db20aea3c7f61d20bc9f844e7baec394c7776
Dominik Purgał <amonsir29@gmail.com>
2023-04-27 21:40:21 +0000

    docs: add documentation to networking/messages.rs
    This commit adds documentation to all methods and enums in the
    server/networking/messages.rs file. The added documentation will make
    it easier for developers to understand the purpose and behavior of the
    code in this module.

--- server/src/networking/messages.rs
+++ server/src/networking/messages.rs
@@ -5,24 +5,47 @@ use serde_json;
 use super::{SendError, channel::Channel};
 
 #[derive(Debug, Serialize, Deserialize)]
+/// This enum contains message types that can be sent to clients. It's designed for sending small messages
+/// when message ordering is crucial and when messages must be delivered. 
+/// 
+/// This enum uses renet reliable channel.
 pub enum ServerMessage {
     RequestConnect(),
     PlayerJoined(String),
+    ConnectionStatus(),
     PONG(),
 }
 
 #[derive(Serialize, Deserialize)]
+/// This enum contains message types that can be sent to clients. It's designed for sending big messages, like map data,
+/// that aren't frequent. It can be used when the message is large and must be delivered. 
+/// 
+/// The message is sliced into multiple chunks so that it can be sent in multiple frames instead of sending all of it in one packet.
+///
+/// This enum uses renet chunk channel.
 pub enum ServerChunkMessage {
 
 }
 
+#[derive(Serialize, Deserialize)]
+/// This enum contains message types that can be sent to clients. It's designed for sending small pieces of data frequently.
+/// This channel is unreliable, so there is a small chance that messages will not be received at all. 
+/// There are no guarantees about the order in which clients will receive messages.
+/// 
+/// This enum uses renet unrealiable channel.
+pub enum ServerFrequentMessage {
+
+}
+
 #[derive(Debug, Serialize, Deserialize)]
+/// Channel for all kinds of messages that can be received from clients.
 pub enum ClientMessage {
     Connect(String),
     PING(),
 }
 
 impl ServerChunkMessage {
+    /// Sends a message directly to one client. Returns Err(SendError::CannotSend) if the message cannot be sent to the client.
     pub fn send(&self, server: &mut RenetServer, id: u64) -> Result<(), SendError> {
         if !server.can_send_message(id, Channel::Chunk.id()) {
             
@@ -37,6 +60,7 @@ impl ServerChunkMessage {
         }
     }
 
+    /// Sends a message to all clients, but first checks if the message can be sent to all clients. If not, the message will not be sent to any client, and the function will return Err(SendError::CannotSend).
     pub fn broadcast(&self, server: &mut RenetServer) -> Result<(), SendError> {
         for id in server.clients_id() {
             if !server.can_send_message(id, Channel::Chunk.id()) {
@@ -50,6 +74,7 @@ impl ServerChunkMessage {
         Ok(())
     }
 
+    /// Sends a message to all clients except one, but first checks if the message can be sent to all clients. If not, the message will not be sent to any client, and the function will return Err(SendError::CannotSend).
     pub fn broadcast_except(&self, server: &mut RenetServer, id: u64) -> Result<(), SendError> {
         for all_id in server.clients_id() {
             if all_id == id {
@@ -69,6 +94,7 @@ impl ServerChunkMessage {
 }
 
 impl ServerMessage {
+    /// Sends a message directly to one client. Returns Err(SendError::CannotSend) if the message cannot be sent to the client.
     pub fn send(&self, server: &mut RenetServer, id: u64) -> Result<(), SendError> {
         if !server.can_send_message(id, Channel::Reliable.id()) {
             return Err(SendError::CannotSend);
@@ -80,6 +106,7 @@ impl ServerMessage {
         Ok(())
     }
 
+    /// Sends a message to all clients, but first checks if the message can be sent to all clients. If not, the message will not be sent to any client, and the function will return Err(SendError::CannotSend).
     pub fn broadcast(&self, server: &mut RenetServer) -> Result<(), SendError> {
         for id in server.clients_id() {
             if !server.can_send_message(id, Channel::Reliable.id()) {
@@ -93,6 +120,7 @@ impl ServerMessage {
         Ok(())
     }
 
+    /// Sends a message to all clients except one, but first checks if the message can be sent to all clients. If not, the message will not be sent to any client, and the function will return Err(SendError::CannotSend).
     pub fn broadcast_except(&self, server: &mut RenetServer, id: u64) -> Result<(), SendError> {
         for id in server.clients_id() {
             if !server.can_send_message(id, Channel::Reliable.id()) {
