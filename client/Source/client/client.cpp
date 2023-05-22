c6c6fbbfaea29aa45bbcff58aed5d59ebb4f2ecf
Amon De Shir <amonsir29@gmail.com>
2023-05-22 04:16:15 +0200

    refactor: remove Unreal Engine 5
    Removing all files related to Unreal Engine 5 to prepare the project
    structure for adopting the Babylon.JS game engine

--- client/Source/client/client.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "client.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, client, "client" );
-
-DEFINE_LOG_CATEGORY(Logclient)
- 
