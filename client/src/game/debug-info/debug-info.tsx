f48349754b6f2987905ac125c2a78789174e8915
Amon De Shir <amonsir29@gmail.com>
2023-05-30 03:38:51 +0200

    refactor: move inspector to the debug-info file
    Sperated inspector related things from main game file to function and moved it to
    the debug-info.tsx file to make code cleaner.
    
    In order to improve code organization and cleanliness,
    we have separated the inspector-related functionalities from the main game file.
    These functionalities have been moved to a separate function within the debug-info.tsx file.
    
    Changes:
        Created the addInspctor function in debug-info.tsx to handle inspector-related tasks.
        Moved the inspector-related code from the main game file to the debug-info.tsx.
        Updated the main game file to utilize the new function for inspector functionalities.

--- client/src/game/debug-info/debug-info.tsx
+++ client/src/game/debug-info/debug-info.tsx
@@ -1,5 +1,6 @@
 import { BabylonWorld } from "../babylon";
 import { Store } from "../../store/store";
+import { Inspector } from '@babylonjs/inspector';
 
 export function updateDebugInfo(world: BabylonWorld) {
   Store.setFps(world.engine.getFps());
@@ -7,3 +8,18 @@ export function updateDebugInfo(world: BabylonWorld) {
   return world;
 }
 
+export function addInspctor(world: BabylonWorld) {
+  Inspector.Show(world.scene, {});
+  world.scene.debugLayer.hide();
+
+  window.addEventListener("keydown", (ev) => {
+    // Shift+Ctrl+Alt+I
+    if (ev.shiftKey &&  ev.key === 'I') {
+      if (world.scene.debugLayer.isVisible()) {
+        world.scene.debugLayer.hide();
+      } else {
+        world.scene.debugLayer.show();
+      }
+    }
+  });
+}
--- client/src/game/game.ts
+++ client/src/game/game.ts
@@ -1,7 +1,6 @@
 import { BabylonWorld, createBabylonWorld } from "./babylon";
 import * as Babylon from "@babylonjs/core";
-import { Inspector } from '@babylonjs/inspector';
-import { updateDebugInfo } from "./debug-info/debug-info";
+import { addInspctor, updateDebugInfo } from "./debug-info/debug-info";
 import { createEffect } from "solid-js";
 import { Store } from "../store/store";
 import { addMap } from "./map/map";
@@ -15,21 +14,7 @@ export async function createGame() {
   let box = Babylon.MeshBuilder.CreateBox("box1", { width: 0.5, height: 0.5, depth: 0.5 }, world.scene);
 
   addMap(world);
-
-  Inspector.Show(world.scene, {});
-  world.scene.debugLayer.hide();
-
-  window.addEventListener("keydown", (ev) => {
-    // Shift+Ctrl+Alt+I
-    if (ev.shiftKey &&  ev.key === 'I') {
-      if (world.scene.debugLayer.isVisible()) {
-        world.scene.debugLayer.hide();
-      } else {
-        world.scene.debugLayer.show();
-      }
-    }
-  });
-
+  addInspctor(world);
 
   const renderLoop = () => {
     updateDebugInfo(world);
