3a987623c4dc32894225e7a237aa4d86946f48f6
Amon De Shir <amonsir29@gmail.com>
2023-06-14 14:56:52 +0200

    feat: add shortcut for automatic login
    - Added shortcut for automatic login and navigation to the main menu.
    - Updated the shortcut for opening the debugLayer to avoid conflicts with Chrome Dev Tools.
    
    These changes will speed up the testing process.

--- client/src/game/debug-info/debug-info.tsx
+++ client/src/game/debug-info/debug-info.tsx
@@ -1,6 +1,7 @@
-import { BabylonWorld } from "../babylon";
+import { BabylonWorld } from "../babylon/babylon";
 import { Store } from "../../store/store";
 import { Inspector } from '@babylonjs/inspector';
+import { Networking } from "../../network/network";
 
 export function updateDebugInfo(world: BabylonWorld) {
   Store.setFps(world.engine.getFps());
@@ -8,18 +9,33 @@ export function updateDebugInfo(world: BabylonWorld) {
   return world;
 }
 
-export function addInspctor(world: BabylonWorld) {
-  Inspector.Show(world.scene, {});
+export function addInspector(world: BabylonWorld) {
+  Inspector.Show(world.scene, { embedMode: true, showExplorer: true, showInspector: true });
   world.scene.debugLayer.hide();
 
   window.addEventListener("keydown", (ev) => {
-    // Shift+Ctrl+Alt+I
-    if (ev.shiftKey &&  ev.key === 'I') {
+    // Shift+O
+    if (ev.shiftKey && ev.key === 'O') {
       if (world.scene.debugLayer.isVisible()) {
+        document.querySelector<HTMLDivElement>("#inspector-host").style.zIndex = "0";
         world.scene.debugLayer.hide();
       } else {
-        world.scene.debugLayer.show();
+        world.scene.debugLayer.show({ showExplorer: true, showInspector: true });
+        document.querySelector<HTMLDivElement>("#inspector-host").style.zIndex = "20";
       }
+    }
+  });
+}
+
+export function addBackToMainMenuShortcut() {
+  window.addEventListener("keydown", (ev) => {
+    // Shift+R
+    if (ev.shiftKey && ev.key === 'R') {
+      if (window.location.pathname !== "/") {
+        Networking.disconnect();
+        window.location.href = "/";
+      }
+
     }
   });
 }
